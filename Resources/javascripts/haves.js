9e7a8d22e9cf78fdbfb6afa0aafbc6033a28d5d1
Joshua Ruihley <ruihley@gmail.com>
2010-01-16 16:22:14 -0500

    added 'haves' view

---
+++ Resources/javascripts/haves.js
@@ -0,0 +1,43 @@
+var tableView;
+var data = [];
+var template = {
+ rowHeight:50,
+ layout:[
+   {type:'text', fontSize:22, fontWeight:'bold', left:10, top:10, width:290, height:40, color:'#222', name:'title'}
+]};
+
+function displayHaves(haves) {
+    for (i in haves) {
+        try {
+            data.push({'title':haves[i].content});
+        } catch(e) {}
+    }
+    var win;
+    tableView = Titanium.UI.createTableView({
+        template:template, 
+        data:data
+        }, function(eventObject) {
+            win = Titanium.UI.createWindow({url:'/category_feed.html', title:eventObject.rowData.title});
+            win.open({animated:true});
+        });
+    Titanium.UI.currentWindow.addView(tableView);
+    Titanium.UI.currentWindow.showView(tableView);
+}
+
+function retrieveHaves() {
+    Item.haves(displayHaves);
+}
+
+window.onload = function(){
+	document.getElementById("loading").style.display = "block";
+	if(Titanium.Platform.name == 'android') {
+		activityIndicator = Titanium.UI.createActivityIndicator();
+		activityIndicator.setMessage('Loading...');
+    activityIndicator.setLocation(Titanium.UI.ActivityIndicator.DIALOG);
+    activityIndicator.setType(Titanium.UI.ActivityIndicator.INDETERMINANT);
+	} else {
+		activityIndicator = Titanium.UI.createActivityIndicator({id:'loading', style:Titanium.UI.iPhone.ActivityIndicatorStyle.BIG});
+	}
+	activityIndicator.show();
+	retrieveHaves();
+};
--- build/iphone/Classes/ApplicationRouting.h
+++ build/iphone/Classes/ApplicationRouting.h
@@ -16,10 +16,12 @@
 }
 - (NSData*) resolveAppAsset:(NSURL*)url;
 - (NSData*) pageNamedCategory_feed;
+- (NSData*) pageNamedHaves;
 - (NSData*) pageNamedNeeds;
 - (NSData*) pageNamedSettings;
 - (NSData*) scriptNamedJavascripts_api;
 - (NSData*) scriptNamedJavascripts_category_feed;
+- (NSData*) scriptNamedJavascripts_haves;
 - (NSData*) scriptNamedJavascripts_jquery_1_4;
 - (NSData*) scriptNamedJavascripts_needs;
 - (NSData*) scriptNamedJavascripts_settings;
