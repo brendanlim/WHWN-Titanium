8f7c4fffdd0fc9658e20d65d922cf4698cf4c07b
Brendan G. Lim <brendangl@gmail.com>
2010-01-16 11:39:54 -0500

    Actually, reverting my last change

--- Resources/javascripts/index.js
+++ Resources/javascripts/index.js
@@ -3,9 +3,7 @@ var tabs = Titanium.UI.getTabs();
 window.onload = function() {
   Titanium.UI.currentWindow.addEventListener('focused',function(e) {
 		setTimeout(function() {
-			if((Titanium.App.Properties.getString("username") == null || Titanium.App.Properties.getString("password") == null)) {
-    		Titanium.UI.setActiveTab(tabs[1]);
-			}
+      
 		},400);
 	});
 };
