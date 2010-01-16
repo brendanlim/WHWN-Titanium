179a69f97b8d4f27e224a31d87144abd4a8dfd02
klondike <kprojection@gmail.com>
2010-01-16 13:34:27 -0500

    Added URL serialization, create-ing would actually work

--- Resources/javascripts/api.js
+++ Resources/javascripts/api.js
@@ -30,6 +30,7 @@
 */
 
 Item = function(fields, format) {
+    if (!fields) fields = {};
     if (!format) format = "json";
     
     this.endpoint = "http://localhost";
@@ -39,62 +40,66 @@ Item = function(fields, format) {
     
     this.create = function() {
         return this.post("/post");
-    }
+    };
     
     this.url = function(path) {
-        return this.endpoint + path + "?test";
-    }
+        return this.endpoint + path + "." + this.format + "?" + $.param(this.fields, true);
+    };
     
     this.get = function(path) {
         response = this.request('GET', this.url(path), format);
         return(response);
-    }
+    };
 
     this.post = function(path) {
         return this.request('POST', this.url(path), format);
-    }
+    };
 
     this.request = function(method, url) {
         if (!method) method = "GET";
-        alert("Would be doing a " + method + " with url: " + url);
         var xhr = Titanium.Network.createHTTPClient();
         xhr.open(method, url);
         xhr.onreadystatechange = function() {
-        try {
-            if (this.readyState == 4) {   
-                if (this.status == 200) {
-                    try {
-                        var results = eval('('+this.responseText+')');
-                        for(var index in results) {
-                            buildData(results[index], index);
+            try {
+                if (this.readyState == 4) {   
+                    if (this.status == 200) {
+                        try {
+                            var results = eval('('+this.responseText+')');
+                            for(var index in results) {
+                                buildData(results[index], index);
+                            }
+                            if(tableView == null) {
+                                var win;
+                                tableView = Titanium.UI.createTableView({
+                                    template:template, 
+                                    data:data
+                                }, function(eventObject) {
+                                    win = Titanium.UI.createWindow({url:'/category_feed.html', title:eventObject.rowData.title});
+                                    win.open({animated:true});
+                                });
+                            Titanium.UI.currentWindow.addView(tableView);
+                            Titanium.UI.currentWindow.showView(tableView);
+                            }           
+                            activityIndicator.hide();
+                            Titanium.UI.currentWindow.repaint();
+                        } catch(ex) {
+                            Titanium.API.error(ex);
+                            var alert = Titanium.UI.createAlertDialog();
+                            alert.setMessage('There was a problem retrieving this list of wants.  Please try again later.');
+                            alert.show();
                         }
-                        if(tableView == null) {
-                            var win;
-                            tableView = Titanium.UI.createTableView({
-                                template:template, 
-                                data:data
-                            }, function(eventObject) {
-                                win = Titanium.UI.createWindow({url:'/category_feed.html', title:eventObject.rowData.title});
-                                win.open({animated:true});
-      						});
-						Titanium.UI.currentWindow.addView(tableView);
-						Titanium.UI.currentWindow.showView(tableView);
-                        }           
+                    } else {
                         activityIndicator.hide();
-                        Titanium.UI.currentWindow.repaint();
-                    } catch(ex) {
-                        Titanium.API.error(ex);
-                        var alert = Titanium.UI.createAlertDialog();
-                        alert.setMessage('There was a problem retrieving this list of wants.  Please try again later.');
-                        alert.show();
                     }
-                } else {
-                    activityIndicator.hide();
-                }
-            }   
-        } catch(excep) {
-            activityIndicator.hide();
-        }
+                }   
+            } catch(excep) {
+                activityIndicator.hide();
+            }
+        };
+        xhr.send();
     };
-    xhr.send();
-    }
+    
+//     this.request = function(method, url) {
+//         alert("Would be doing a " + method + " with url: " + url);
+//     };
+}
