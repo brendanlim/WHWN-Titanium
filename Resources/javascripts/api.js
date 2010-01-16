f03ab805cc088ac089dcc534cea318afe5fcff07
klondike <kprojection@gmail.com>
2010-01-16 14:51:35 -0500

    Moved display callbacks into api.js

--- Resources/javascripts/api.js
+++ Resources/javascripts/api.js
@@ -114,3 +114,16 @@ Item = {
         }
     }
 }
+
+ItemDisplay = {
+    displayError: function(ex) {
+        Titanium.API.error(ex);
+        var alert = Titanium.UI.createAlertDialog();
+        alert.setMessage('There was a problem retrieving this list of wants.  Please try again later.');
+        alert.show();      
+    },
+    
+    httpError: function(status) {
+        alert(status + ": request failed");
+    }
+};
